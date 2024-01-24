1020ebdd11cedddf8ec3713ee9aadd7dee6f90bb
User <User@LAB3-3>
2024-01-24 09:32:46 +0200

    funn

--- binding/MainPage.xaml.cs
+++ binding/MainPage.xaml.cs
@@ -1,14 +1,21 @@
-﻿namespace binding
+﻿using binding.Models;
+
+namespace binding
 {
     public partial class MainPage : ContentPage
     {
-   
+   public Student student { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+            student = new Student() { Name = "Shahar", Age = 16 };
+            this.BindingContext = student;
         }
-
+        private void ChangeName(object sender, EventArgs e)
+        {
+            student.Name = "נועה";
+        }
      
     }
 }
